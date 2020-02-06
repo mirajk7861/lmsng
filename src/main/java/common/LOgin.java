9b9966b0781cc65a17acf3373ba504565a52081b
mirajk7861 <58694299+mirajk7861@users.noreply.github.com>
2020-02-06 22:36:05 +0530

    Update LOgin.java

--- src/main/java/common/LOgin.java
+++ src/main/java/common/LOgin.java
@@ -16,6 +16,25 @@ public class LOgin {
 	public static void login(String environment) throws InterruptedException
 	{
 		
-		System.out.println("PASS");
+		DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
+		/****capability.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		capability.setCapability("requireWindowFocus", true);****/
+		capability.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		capability.setCapability("requireWindowFocus", true);
+		capability.setCapability("ignoreZoomSetting", true);			
+		//System.out.println("capability::::"+capability);
+		System.setProperty("webdriver.ie.driver","C:\\src\\test\\resources\\IEDriverServer.exe");
+		System.out.println("C:\\src\\test\\resources\\IEDriverServer.exe");
+		Thread.sleep(3000);
+		driver=new InternetExplorerDriver(capability);		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.navigate().to("http://www.google.com");
+		System.out.println("google open");
+		driver.findElement(By.tagName("html")).sendKeys(Keys.chord(Keys.CONTROL, "0"));
+		//driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 }
